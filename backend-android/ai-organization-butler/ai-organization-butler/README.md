adb1d3476f38cc619c58327755dee856c46b1bb0
lakshyajais29 <lakshyajaiswal2929@gmail.com>
2026-01-27 16:33:58 +0530

    Flutter app with Roboflow ML

---
+++ backend-android/ai-organization-butler/ai-organization-butler/README.md
@@ -0,0 +1,2 @@
+# ai-organization-butler
+AI-powered room organizer: detects objects, scores messiness, and generates step-by-step cleaning plans.
---
+++ backend-android/ai-organization-butler/ai-organization-butler/backend-android/android/app/src/main/kotlin/com/example/ai_organization_butler/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ai_organization_butler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
